67ee888aa60bf59ecaade0373e5b93f17193c7ad
Ege Ugur Agus <48795506+duffydrunk@users.noreply.github.com>
2022-05-25 22:59:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import pygame, sys
+from defaults import *
+from level import Level
+
+class Game:
+    def __init__(self):
+        
+        #
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption("Beamer")
+        self.clock = pygame.time.Clock()
+        
+        self.level = Level()
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+            self.screen.fill("black")
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+            
+if __name__ == "__main__":
+    game = Game()
+    game.run()
